e991882886657078afcb8518d59589a73c671b85
quytm <quytm_58@vnu.edu.vn>
2016-06-30 22:54:50 +0700

    Tối giản phương thức connect tới database

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var mysql = require('mysql');
 var router = express.Router();
 
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Danh sách sinh viên' });
@@ -25,49 +27,39 @@ var config = {
   password: '1234',
   database: 'qly_svien_node'
 };
+var connection = mysql.createConnection(config);
+connection.connect(function (err) {
+  if (err){
+
+  }
+});
 
 router.get('/api/listsv', function(req, res, next) {
-  var connection = mysql.createConnection(config);
-  connection.connect();
-
   connection.query('SELECT * FROM sinhvien', function (err, rows, fields) {
     if (err) throw err;
     res.json(rows);
   });
-
-  connection.end();
-
 });
 
 router.get('/api/sv/:mssv', function (req, res) {
-  var connection = mysql.createConnection(config);
-  connection.connect();
-  // console.log('Mssv = ' + req.params.mssv);
+  var query = 'SELECT * FROM sinhvien WHERE mssv = "' + req.params.mssv + '"';
+  console.log('Query = ' + query);
 
-  connection.query('SELECT * FROM sinhvien WHERE mssv = ' + req.params.mssv, function (err, rows, fields) {
+  connection.query(query, function (err, rows, fields) {
     if (err) throw err;
     res.json(rows);
   });
-
-  connection.end();
 });
 
 router.get('/api/delete/:mssv', function (req, res) {
-  var connection = mysql.createConnection(config);
-  connection.connect();
-
   connection.query('DELETE FROM sinhvien WHERE mssv = ' + req.params.mssv, function (err, rows, fields) {
     if (err) throw err;
   });
 
-  connection.end();
   res.end('deleted');
 });
 
 router.post('/api/add_new', function (req, res) {
-  var connection = mysql.createConnection(config);
-  connection.connect();
-
   var query = 'INSERT INTO `sinhvien` (`mssv`, `hoten`, `ngaysinh`, `tinchi`) VALUES (' +
       '"' + req.body.mssv + '",' +
       '"' + req.body.hoten + '",' +
@@ -84,20 +76,10 @@ router.post('/api/add_new', function (req, res) {
     console.log(result);
   });
 
-  connection.end();
   res.end('Complete');
 });
 
 router.post('/api/edit', function (req, res) {
-  var connection = mysql.createConnection(config);
-  connection.connect();
-
-  // var query = 'INSERT INTO `sinhvien` (`mssv`, `hoten`, `ngaysinh`, `tinchi`) VALUES (' +
-  //     '"' + req.body.mssv + '",' +
-  //     '"' + req.body.hoten + '",' +
-  //     '"' + req.body.ngaysinh + '",' +
-  //     '"' + req.body.tinchi + '"' +
-  //     ');';
   var query = ' UPDATE sinhvien SET ' +
       'hoten = "' + req.body.hoten + '", ' +
       'ngaysinh = "' + req.body.ngaysinh + '", ' +
@@ -113,7 +95,6 @@ router.post('/api/edit', function (req, res) {
     console.log(result);
   });
 
-  connection.end();
   res.end('Complete');
 });
 
